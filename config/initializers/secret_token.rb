439bc59b45bf660277b3d87eb4e599bf1751e6b4
ayushmad <ayushmad@usc.edu>
2014-02-18 16:18:31 -0800

    First Rails Base app strucutre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternetArchiveVisualization::Application.config.secret_token = '834dc82db3df12c6056be7ae5ae94ac7fcea8d655757df642faf1ad05c8d8714e28c90ab9a9bd44df40eea50159697165d274e1379a0119f9aa8d9baaeb4da3b'
